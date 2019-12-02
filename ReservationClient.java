c474492f21ca1ed6c4c4d97166a46772b7d087d9
asarpoli <54542743+asarpoli@users.noreply.github.com>
2019-12-02 17:33:11 -0500

    Update ReservationClient.java

--- ReservationClient.java
+++ ReservationClient.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
 
 /**
  * CS180 - Project 05
  * <p>
- * ReservationClient.java Class; Handles reservationclient
+ * ReservationClient.java Class; Handles reservationClient
  * Received some help from past homeworks and labs
  *
  * @author Alexandra Sarpolis, asarpoli@purdue.edu; Ray Ye, yer@purdue.edu
- * @version 11/21/2019
+ * @version 12/2/2019
  */
 public class ReservationClient {
 
@@ -21,16 +20,10 @@ public class ReservationClient {
                 .orElse(Boolean.FALSE);
     }
 
-    public static void main(String[] args) {
-        BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws InterruptedException {
+
         String hostname;
         String portString;
-        int port;
-        Socket socket;
-        BufferedWriter socketWriter = null;
-        BufferedReader socketReader = null;
-        String request;
-        String response;
 
         try {
             hostname = (String) JOptionPane.showInputDialog(null, "Enter the sever's hostname: ",
@@ -54,30 +47,9 @@ public class ReservationClient {
                             "Error", JOptionPane.WARNING_MESSAGE);
 
                 } else {
+
                     ReservationGui.getIntroFlightSemantics(hostname, portString);
 
-
-                    socket = new Socket(hostname, Integer.parseInt(portString));
-                    socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                    socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    System.out.println();
-                    request = userInputReader.readLine();
-
-                    socketWriter.write(request);
-                    socketWriter.newLine();
-                    socketWriter.flush();
-                    response = socketReader.readLine();
-                    System.out.println();
-                    System.out.printf("Response from server: %s%n", response);
-                    System.out.println();
-                    System.out.print("Enter your request: ");
-                    request = userInputReader.readLine();
-
-
-
-
-                    JOptionPane.showMessageDialog(null, "Goodbye!", "Goodbye!",
-                            JOptionPane.PLAIN_MESSAGE);
                 }
             }
         } catch (IOException e) {
