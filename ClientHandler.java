4cc4bb85b5b90584d4874970a9ed5c7bed8bf39c
asarpoli <54542743+asarpoli@users.noreply.github.com>
2019-12-02 12:26:11 -0500

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,13 +1,9 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
+import java.util.ArrayList;
 import java.util.Objects;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  * CS180 - Project 05
@@ -22,6 +18,8 @@ import java.util.logging.Logger;
 public final class ClientHandler extends Thread implements Runnable {
 
     private Socket clientSocket;
+    private ObjectInputStream inputObjectStream = null;
+    private ObjectOutputStream outputObjectStream = null;
 
     public ClientHandler(Socket clientSocket) throws NullPointerException {
         Objects.requireNonNull(clientSocket, "the specified client socket is null");
@@ -31,6 +29,60 @@ public final class ClientHandler extends Thread implements Runnable {
 
     @Override
     public void run() {
+        Delta d = new Delta();
+        Alaska a = new Alaska();
+        Southwest s = new Southwest();
+        ArrayList<String> fileContents = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("reservations.txt"));
+
+            while(true) {
+                String line = br.readLine();
+                if (line == null)
+                    break;
+                fileContents.add(line);
+            }
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+
+        int alaskaIndex = 0;
+        int deltaIndex = 0;
+        int southwestIndex = 0;
+        for (int i = 0; i < fileContents.size(); i++)
+        {
+            if (fileContents.get(i).contains("ALASKA"))
+                alaskaIndex = i;
+            if (fileContents.get(i).contains("DELTA"))
+                deltaIndex = i;
+            if (fileContents.get(i).contains("SOUTHWEST"))
+                southwestIndex = i;
+        }
+
+        String alaskaAmount = fileContents.get(alaskaIndex + 2).substring(0, 1);
+        a.setSpotsTaken(Integer.parseInt(alaskaAmount));
+        String deltaAmount = fileContents.get(deltaIndex + 2).substring(0, 1);
+        d.setSpotsTaken(Integer.parseInt(deltaAmount));
+        String southwestAmount = fileContents.get(southwestIndex + 2).substring(0, 1);
+        s.setSpotsTaken(Integer.parseInt(southwestAmount));
+
+        for (int i = alaskaIndex; i < deltaIndex; i++)
+        {
+            if (!fileContents.get(i).equals("") && fileContents.get(i).contains("."))
+                a.getPassengers().add(fileContents.get(i));
+        }
+
+        for (int i = deltaIndex; i < southwestIndex; i++)
+        {
+            if (!fileContents.get(i).equals("") && fileContents.get(i).contains("."))
+                d.getPassengers().add(fileContents.get(i));
+        }
+
+        for (int i = southwestIndex; i < fileContents.size(); i++)
+        {
+            if (!fileContents.get(i).equals("") && fileContents.get(i).contains("."))
+                s.getPassengers().add(fileContents.get(i));
+        }
         /*
         try {
 
@@ -51,6 +103,22 @@ public final class ClientHandler extends Thread implements Runnable {
         }
 
          */
+
+        try{
+
+            outputObjectStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            outputObjectStream.writeObject(a);
+            outputObjectStream.flush();
+            outputObjectStream.writeObject(d);
+            outputObjectStream.flush();
+            outputObjectStream.writeObject(s);
+            outputObjectStream.flush();
+
+            outputObjectStream.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
     }
 
