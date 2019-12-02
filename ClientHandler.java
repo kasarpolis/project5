03eb9f91d3129f9e470185578c29ddbc836b5195
asarpoli <54542743+asarpoli@users.noreply.github.com>
2019-12-02 14:33:32 -0500

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -33,10 +33,12 @@ public final class ClientHandler extends Thread implements Runnable {
         Alaska a = new Alaska();
         Southwest s = new Southwest();
         ArrayList<String> fileContents = new ArrayList<>();
+        Airline air = null;
+        Passenger p;
         try {
             BufferedReader br = new BufferedReader(new FileReader("reservations.txt"));
 
-            while(true) {
+            while (true) {
                 String line = br.readLine();
                 if (line == null)
                     break;
@@ -49,8 +51,7 @@ public final class ClientHandler extends Thread implements Runnable {
         int alaskaIndex = 0;
         int deltaIndex = 0;
         int southwestIndex = 0;
-        for (int i = 0; i < fileContents.size(); i++)
-        {
+        for (int i = 0; i < fileContents.size(); i++) {
             if (fileContents.get(i).contains("ALASKA"))
                 alaskaIndex = i;
             if (fileContents.get(i).contains("DELTA"))
@@ -66,45 +67,23 @@ public final class ClientHandler extends Thread implements Runnable {
         String southwestAmount = fileContents.get(southwestIndex + 2).substring(0, 1);
         s.setSpotsTaken(Integer.parseInt(southwestAmount));
 
-        for (int i = alaskaIndex; i < deltaIndex; i++)
-        {
+        for (int i = alaskaIndex; i < deltaIndex; i++) {
             if (!fileContents.get(i).equals("") && fileContents.get(i).contains("."))
                 a.getPassengers().add(fileContents.get(i));
         }
 
-        for (int i = deltaIndex; i < southwestIndex; i++)
-        {
+        for (int i = deltaIndex; i < southwestIndex; i++) {
             if (!fileContents.get(i).equals("") && fileContents.get(i).contains("."))
                 d.getPassengers().add(fileContents.get(i));
         }
 
-        for (int i = southwestIndex; i < fileContents.size(); i++)
-        {
+        for (int i = southwestIndex; i < fileContents.size(); i++) {
             if (!fileContents.get(i).equals("") && fileContents.get(i).contains("."))
                 s.getPassengers().add(fileContents.get(i));
         }
-        /*
+
+
         try {
-
-            PrintWriter pw = new PrintWriter("reservations.txt");
-            ReservationGui.getIntroFlightSemantics();
-
-            while (ReservationGui.getPassenger() != null) {
-                String line = in.nextLine();
-                System.out.printf("%s says: %s\n", clientSocket, line);
-                pw.printf("echo: %s\n", line);
-                pw.flush();
-            }
-
-            pw.close();
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-         */
-
-        try{
 
             outputObjectStream = new ObjectOutputStream(clientSocket.getOutputStream());
             outputObjectStream.writeObject(a);
@@ -114,12 +93,74 @@ public final class ClientHandler extends Thread implements Runnable {
             outputObjectStream.writeObject(s);
             outputObjectStream.flush();
 
-            outputObjectStream.close();
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
+        try {
+            inputObjectStream = new ObjectInputStream(clientSocket.getInputStream());
+            Object o = inputObjectStream.readObject();
+            if (o instanceof Alaska)
+                a = (Alaska) o;
+            if (o instanceof Delta)
+                d = (Delta) o;
+            if (o instanceof Delta)
+                s = (Southwest) o;
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            outputObjectStream.close();
+            inputObjectStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        /*
+        try {
+            File tempFile = new File();
+            FileOutputStream fos = new FileOutputStream(tempFile, false);
+            PrintWriter pw = new PrintWriter(fos);
+
+            pw.println("ALASKA");
+            pw.println("-------------------------------");
+            pw.println(a.getSpotsTaken() + "/100");
+            for (int i = 0; i < a.getPassengers().size(); i++) {
+                pw.println(a.getPassengers().get(i));
+            }
+            pw.println();
+            pw.println("DELTA");
+            pw.println("-------------------------------");
+            pw.println(d.getSpotsTaken() + "/100");
+            for (int i = 0; i < d.getPassengers().size(); i++) {
+                pw.println(d.getPassengers().size());
+            }
+            pw.println();
+            pw.println("SOUTHWEST");
+            pw.println("-------------------------------");
+            pw.println(s.getSpotsTaken() + "/100");
+            for (int i = 0; i < s.getPassengers().size(); i++) {
+                pw.println(s.getPassengers().get(i));
+            }
+
+            File f1 = new File("reservations.txt");
+            boolean b = f1.delete();
+            System.out.println(b);
+
+            if (b) {
+                File f2 = new File(tempFile.getAbsolutePath());
+                File f3 = new File("reservations.txt");
+                boolean c = f2.renameTo(f3);
+            }
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+         */
     }
 
 }
